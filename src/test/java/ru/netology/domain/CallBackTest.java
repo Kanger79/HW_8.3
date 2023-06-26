875706b834f731cf2bf206e0715d7fe8e1613115
Dmitry Kanger <dmitrykanger@yandex.ru>
2023-06-26 20:50:41 +0300

    fix app-order

--- src/test/java/ru/netology/domain/CallBackTest.java
+++ src/test/java/ru/netology/domain/CallBackTest.java
@@ -1,22 +1,33 @@
 package ru.netology.domain;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class CallBackTest {
-    private WebDriver driver;
+    private static WebDriver driver;
+
+    static ChromeOptions options = new ChromeOptions();
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+        WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @BeforeEach
     void setUp() {driver=new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
     }
     @AfterEach
     void tearDown() {
@@ -27,6 +38,7 @@ public class CallBackTest {
     @Test
     void shouldTestForm() {
         // Загрузить страницу
+        driver.get ("http://localhost:9999");
         // Поиск элементов
         // Взаимодействие с элементами
 
